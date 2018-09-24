81eafe647130f71261c9bb121a5cdc90c395e313
Rageeb Mahtab <>
2018-09-24 02:21:56 -0400

    Added the Business Cell Class and the Views connected to the API

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,44 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Rageeb Mahtab on 9/24/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewsLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var foodTypeLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbnailImageView.setImageWith(business.imageURL!)
+            foodTypeLabel.text = business.categories
+            reviewsLabel.text = "\(business.reviewCount!) Reviews"
+            addressLabel.text = business.address
+            ratingImageView.image = business.ratingImage
+            distanceLabel.text = business.distance
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
